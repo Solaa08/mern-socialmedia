024b27f2e7b55201e657b01a4d7c1cc67b549a52
Solaa08 <solarium_08@hotmail.fr>
2023-01-12 20:49:02 +0100

    Loggin register working + middlewares

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -38,3 +38,22 @@ export const register = async (req, res) => {
         res.status(500).json({ error: err.message })
     }
 }
+
+// LOGGING
+
+export const login = async (req, res) => {
+    try {
+        const { email, password } = req.body
+        const user = await User.findOne({ email: email })
+        if (!user) return res.status(400).json({ msg: "L'utilisateur n'existe pas. "})
+
+        const isMatch = await bcrypt.compare(password, user.password)
+        if (!isMatch) return res.status(400).json({ msg: "Identifiants incorrect"})
+
+        const token = jwt.sign({ id:user._id }, process.env.JWT_SECRET)
+        delete user.password
+        res.status(200).json({ token, user })
+    } catch (err) {
+        res.status(500).json({ error: err.message })
+    }
+}
--- server/index.js
+++ server/index.js
@@ -11,6 +11,7 @@ import morgan from "morgan"
 import path from "path"
 import { fileURLToPath } from "url"
 import authRoutes from "./routes/auth.js"
+import userRoutes from "./routes/users.js"
 import { register } from "./controllers/auth.js"
 
 // CONFIG
@@ -44,6 +45,11 @@ const upload = multer({ storage })
 // FILE ROUTES
 
 app.post("/auth/register", upload.single("picture"), register)
+
+// ROUTES
+
+app.use("/auth", authRoutes)
+app.use("/users", userRoutes)
 
 // MONGOOSE 
 
