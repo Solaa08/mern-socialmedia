bc5bf6818ee61b75557228a9b571d46f50cf173e
Solaa08 <solarium_08@hotmail.fr>
2023-01-17 13:50:59 +0100

    Setting up frontend & routes

--- server/index.js
+++ server/index.js
@@ -16,6 +16,9 @@ import postRoutes from "./routes/posts.js"
 import { register } from "./controllers/auth.js"
 import { createPost } from "./controllers/posts.js"
 import { verifyToken } from "./middleware/auth.js"
+import User from "./models/User.js"
+import Post from "./models/Post.js"
+import { users, posts } from "./data/index.js"
 
 // CONFIG
 
@@ -64,4 +67,7 @@ mongoose.connect(process.env.MONGO_URL, {
   useUnifiedTopology: true,
 }).then(() => {
   app.listen(PORT, () => console.log('Server running on port 3001'))
+
+  // User.insertMany(users)
+  // Post.insertMany(posts)
 }).catch((error) => console.log(`${error} did not connect`))
